dfb8ddfbe986e306fd4363a8851595f64dba8fb2
Mónica <84694323+Acinom30@users.noreply.github.com>
2023-12-05 18:13:56 -0600

    Add files via upload

---
+++ Examen2/src/main/java/cr/go/ucr/Main.java
@@ -0,0 +1,33 @@
+package cr.go.ucr;
+
+import cr.go.ucr.operaciones.Division;
+import cr.go.ucr.operaciones.Multiplicacion;
+import cr.go.ucr.operaciones.Resta;
+import cr.go.ucr.operaciones.Sumar;
+
+public class Main {
+
+	public static void main(String[] args) {
+	   OperacionAritmetica context;
+       context = new OperacionAritmetica( new Sumar() );
+       int suma = context.procesar(5,10);
+       context = new OperacionAritmetica( new Resta() );
+       int resta = context.procesar(20,4);
+       context = new OperacionAritmetica( new Multiplicacion() );
+       int multip = context.procesar(5,4);
+       context = new OperacionAritmetica( new Division() );
+       int divis = context.procesar(5,4);
+       
+
+       
+       System.out.println("Suma: " + suma );
+       System.out.println("Resta: " + resta );
+       System.out.println("Multiplicación: " + multip );
+       System.out.println("División: " + divis );
+   
+       
+	}
+
+}
+
+}
